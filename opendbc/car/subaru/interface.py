b014f75c81afda5c186e42e7d910b73a76079507
Mech Tech <mechtechteam@gmail.com>
2025-08-08 19:29:58 -0400

    Update interface.py

--- opendbc/car/subaru/interface.py
+++ opendbc/car/subaru/interface.py
@@ -63,12 +63,12 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in ( CAR.SUBARU_CROSSTREK_2024, CAR.SUBARU_CROSSTREK_2025 ):
       ret.dashcamOnly = False
-      ret.steerActuatorDelay = 0.3
-      ret.steerActuatorDelay = 0.4  # end-to-end angle controller
+      #ret.steerActuatorDelay = 0.3
+      ret.steerActuatorDelay = 0.2  # end-to-end angle controller
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kf = 0.00005
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 14., 23.], [0., 14., 23.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.045, 0.042, 0.20], [0.04, 0.035, 0.045]]
 
     elif candidate == CAR.SUBARU_IMPREZA:
       ret.steerActuatorDelay = 0.4  # end-to-end angle controller
